b480fb9c5e02ed2e5c0007cd22990362e26feaa5
pluu <pluulove@gmail.com>
2025-09-27 12:11:33 +0900

    Enable edge to edge

--- app/src/main/java/com/pluu/sample/logger/MainActivity.kt
+++ app/src/main/java/com/pluu/sample/logger/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.pluu.sample.logger
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.pluu.logger.CoreLogger
 import com.pluu.logger.analytics
 import com.pluu.logger.crashlytics
@@ -14,8 +17,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         binding.btnCrashlytics.setOnClickListener {
             CoreLogger.firebase
